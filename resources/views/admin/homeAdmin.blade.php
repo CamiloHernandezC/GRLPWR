f5371621e75d3b5a8f5fe8264cdd1fe75e60e9ba
Camilo Hernandez <camehernandezcas@unal.edu.co>
2023-10-10 09:06:00 -0500

    removing redundant code

--- resources/views/admin/homeAdmin.blade.php
+++ resources/views/admin/homeAdmin.blade.php
@@ -7,8 +7,6 @@
 
 @section('modals')
 
-    <link rel="stylesheet" href="{{asset('css/chats.css')}}">
-
     <!--solicitud modal ELIMINAR-->
     <!--Solo se utiliza para clientes ya que los entrenadores pueder ir a la info de la solictud y eliminar su oferta-->
     <div class="modal fade" id="solicitudModalEliminar" tabindex="-1" role="dialog">
@@ -51,11 +49,3 @@
         @include('proximasSesiones')
     </div>
 @endsection
-
-    <div class="floating_button">
-        <div class="chats">
-            <a href="https://api.whatsapp.com/send/?phone=573123781174<&text=Hola,%20quisiera%20recibir%20mas%20informacion&app_absent=0" class="icon-whatsapp" target=”_blank”>
-                <img class="icon" width="100%" height="100%" alt="whatsapp" src="<?php echo e(asset('images/wathsapp_icon.png')); ?>">
-            </a>
-        </div>
-    </div>
