445ec8a6ab7c233be968c0d386385b25ded751e1
Camilo Hernandez <camehernandecas@unal.edu.co>
2023-10-16 11:40:39 -0500

    new convertion measure

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -50,23 +50,6 @@
         gtag('config', '{{env('GTAG')}}');
     </script>
 
-    <!-- Event snippet for Registro conversion page
-        In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
-    <script>
-        function gtag_report_conversion(url) {
-            var callback = function () {
-                if (typeof(url) != 'undefined') {
-                    window.location = url;
-                }
-            };
-            gtag('event', 'conversion', {
-                'send_to': 'AW-780220913/8DZwCOa_i40BEPHzhPQC',
-                'event_callback': callback
-            });
-            return false;
-        }
-    </script>
-
     @stack('head-content')
 </head>
 <body>
@@ -134,17 +117,6 @@
     </script>
     <script type="text/javascript" src="https://cdn.subscribers.com/assets/subscribers.js"></script>
     -->
-
-    <!--google analytics-->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-128937544-1', 'auto');
-        ga('send', 'pageview');
-    </script>
 
 </body>
 </html>
