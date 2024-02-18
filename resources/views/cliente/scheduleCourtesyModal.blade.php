5d6c51ca8d2977424832a68a6bdcb09902382f37
Camilo Hernandez <camehernandezcas@unal.edu.co>
2024-02-18 16:47:24 -0500

    Update scheduleCourtesyModal.blade.php

--- resources/views/cliente/scheduleCourtesyModal.blade.php
+++ resources/views/cliente/scheduleCourtesyModal.blade.php
@@ -5,7 +5,7 @@
 <!-- CSS Files -->
 <link href="{{asset('css/profileWizard.css')}}" rel="stylesheet"/>
 <link href="{{asset('css/scheduleCourtesyModal.css')}}" rel="stylesheet"/>
-
+<!-- Fonts and icons -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 
