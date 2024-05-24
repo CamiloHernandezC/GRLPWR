4334e73cb30aa74e6443564923c3bedbb505efbb
Camilo Hernandez <camehernandezcas@unal.edu.co>
2024-05-24 14:38:02 -0500

    Add css class and parameterizing values

--- app/Http/Controllers/AchievementController.php
+++ app/Http/Controllers/AchievementController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Assada\Achievements\Model\AchievementProgress;
-
+use Illuminate\Support\Env;
+use Illuminate\Support\Traits\EnumeratesValues;
 
 
 class AchievementController extends Controller
 {
     public function showAchievements()
     {
-        $achievements = AchievementProgress::where('achievement_id', 7)->with('achiever')->orderBy('points', 'desc')->take(10)->get();
+        $achievements = AchievementProgress::where('achievement_id', env('WEEKS_TRAINED_ACHIEVEMENT_ID'))->with('achiever')->orderBy('points', 'desc')->take(10)->get();
         return view('cliente.weekachievements', compact('achievements'));
     }
 }
--- resources/views/cliente/weekAchievements.blade.php
+++ resources/views/cliente/weekAchievements.blade.php
@@ -1,51 +1,25 @@
-<!-- resources/views/achievements/index.blade.php -->
+<!-- resources/views/cliente/weekachievements.blade.php -->
 
 @extends('layouts.app')
 
 @section('title', 'Ranking semanas completadas')
 
 @section('content')
-    <style>
-        .table-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-top: 20px;
-        }
-        table {
-            width: 80%;
-            max-width: 800px;
-            border-collapse: collapse;
-            margin-bottom: 20px;
-        }
-        th, td {
-            border: 1px solid #dddddd;
-            text-align: center;
-            padding: 12px;
-            font-size: 16px;
-        }
-        th {
-            background-color: #f2f2f2;
-            font-weight: bold;
-        }
-        tr:nth-child(even) {
-            background-color: #f9f9f9;
-        }
-    </style>
 
+    <link rel="stylesheet" href="{{ asset('css/ranking.css') }}">
     <h1>Ranking semanas completadas</h1>
     <div class="table-container">
         <table>
             <thead>
             <tr>
-                <th>Guerrera</th>
+                <th>Girly</th>
                 <th>Semanas seguidas completadas</th>
             </tr>
             </thead>
             <tbody>
             @foreach ($achievements as $achievement)
                 <tr>
-                    <td>{{ $achievement->achiever->fullName }}</td>
+                    <td>{{ $achievement->achiever->nombre }}</td>
                     <td>{{ $achievement->points }}</td>
                 </tr>
             @endforeach
