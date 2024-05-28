a40406daf5373a3beed62c5acd9980a65eb1d673
Camilo Hernandez <camehernandezcas@unal.edu.co>
2024-05-28 11:59:21 -0500

    Adding minor box valitaion to table transacciones pagos

--- app/Http/Controllers/PagosController.php
+++ app/Http/Controllers/PagosController.php
@@ -151,6 +151,7 @@ class PagosController extends Controller
         $transaction->created_at = $payDay;
         $transaction->cxp = $request->has('cxp') ? 1 : 0;
         $transaction->amount = $request->cxp ? -1*abs($request->amount) : $request->amount;
+        $transaction->caja_menor = 1;
         $transaction->save();
 
         Session::put('msg_level', 'success');
