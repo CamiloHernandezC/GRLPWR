9892f3376d1d686271823f43294ee6091f1a065a
Camilo Hernandez <camehernandecas@unal.edu.co>
2025-04-17 10:06:45 -0500

    fix enddate

--- app/View/Composers/HistoricActiveClientsComposer.php
+++ app/View/Composers/HistoricActiveClientsComposer.php
@@ -113,7 +113,7 @@ class HistoricActiveClientsComposer
         $plansSummary = DB::table('client_plans')
             ->select('plans.name', DB::raw('COUNT(*) as plan_count'))
             ->join('plans', 'plans.id', '=', 'client_plans.plan_id')
-            ->where('client_plans.expiration_date', '>=', $endDate->endOfDay())
+            ->where('client_plans.expiration_date', '>=', $endDate)
             ->groupBy('plans.name')
             ->orderByDesc('plan_count')
             ->get();
